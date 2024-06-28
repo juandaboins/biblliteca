a3a2d4c1d5a246ccaa7434137a726cf17f8d6603
juandaboin <juandaboins@gmail.com>
2024-06-28 11:49:29 -0300

    se creo la clase Libro y biblioteca y se inicio con la estructura de biblioteca

---
+++ src/Biblioteca.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+
+public class Biblioteca {
+    private ArrayList<Libro> libros;
+
+    public Biblioteca(){
+        this.libros = new ArrayList<Libro>();
+    }
+
+    public void agregarLibro(Libro libro) {}
+
+    public Libro buscarLibro(String titulo){}
+
+    public void listarTodos(){}
+}
+
