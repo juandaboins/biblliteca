75ffe9b02e5f7fbcd560a414410f2a4900afb201
juan daboin <juandaboins@gmail.com>
2024-06-30 15:35:11 -0300

    agregamos los atributos, constructor y metodos en la clase Libro

--- src/Biblioteca.java
+++ src/Biblioteca.java
@@ -1,8 +1,11 @@
 import java.util.ArrayList;
 
 public class Biblioteca {
+
+    /*Atributo (ArrayList<>)*/
     private ArrayList<Libro> libros;
 
+    /*Metodos*/
     public Biblioteca(){
         this.libros = new ArrayList<Libro>();
     }
--- src/Libro.java
+++ src/Libro.java
@@ -1,3 +1,50 @@
 public class Libro {
 
+    /*Atributos*/
+    private Integer id;
+    private String titulo;
+    private String autor;
+    private Integer numeroPagina;
+
+    /*Constructor*/
+    public Libro(Integer id, String titulo, String autor, Integer numeroPagina) {
+        this.id = id;
+        this.titulo = titulo;
+        this.autor = autor;
+        this.numeroPagina = numeroPagina;
+    }
+
+    /*Metodos*/
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public Integer getNumeroPagina() {
+        return numeroPagina;
+    }
+
+    public void setNumeroPagina(Integer numeroPagina) {
+        this.numeroPagina = numeroPagina;
+    }
 }
